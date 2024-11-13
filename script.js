bc1862dfb79523c23733b2dc45eaa9cf956d1057
𝙼𝚘𝚞𝚛𝚢𝚊 𝙶𝚘𝚠𝚍𝚊 <163443938+mouryas-aiml@users.noreply.github.com>
2024-11-13 12:41:25 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+function scrollToSection(sectionId) {
+    document.getElementById(sectionId).scrollIntoView({ behavior: 'smooth' });
+}
+
+document.getElementById('contactForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+    alert("Message Sent! We'll get back to you soon.");
+});
